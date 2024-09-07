851c76082decbf767957b11ebe17f0482ee2db37
AlessonBarrosRepositorio <84032869+AlessonBarrosRepositorio@users.noreply.github.com>
2024-09-07 15:38:05 -0300

    js e css

---
+++ src/scripts/focoNave.js
@@ -0,0 +1,15 @@
+function quemSou() {
+    document.getElementById('quemSou').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+function servico() {
+    document.getElementById('servico').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+function objetivo() {
+    document.getElementById('objetivo').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
